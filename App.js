516c801875fcc0cdb6600aef854cbc4e3091ea89
rudarabello <ruda.rabello.silva@gmail.com>
2023-03-01 10:29:20 -0300

    feat: add calculator logic

--- App.js
+++ App.js
@@ -14,13 +14,54 @@ const initialState = {
 export default class App extends Component {
   state = {...initialState};
 
-  addDigit = n => {};
+  addDigit = n => {
+    const clearDisplay =
+      this.state.displayValue === '0' || this.state.clearDisplay;
+
+    if (n === '.' && !clearDisplay && this.state.displayValue.includes('.')) {
+      return;
+    }
+
+    const currentValue = clearDisplay ? '' : this.state.displayValue;
+    const displayValue = currentValue + n;
+    this.setState({displayValue, clearDisplay: false});
+
+    if (n !== '.') {
+      const newValue = parseFloat(displayValue);
+      const values = [...this.state.values];
+      values[this.state.current] = newValue;
+      this.setState({values});
+    }
+  };
 
   clearMemory = () => {
     this.setState({...initialState});
   };
 
-  setOperation = operation => {};
+  setOperation = operation => {
+    if (this.state.current === 0) {
+      this.setState({operation, current: 1, clearDisplay: true});
+    } else {
+      const equals = operation === '=';
+      const values = [...this.state.values];
+      try {
+        // eslint-disable-next-line no-eval
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`);
+      } catch (e) {
+        values[0] = this.state.values[0];
+      }
+
+      values[1] = 0;
+      this.setState({
+        displayValue: `${values[0]}`,
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        //clearDisplay: !equals,
+        clearDisplay: true,
+        values,
+      });
+    }
+  };
 
   render() {
     return (
